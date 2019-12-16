bc3db5d88dbb280c74e2c8eddc8b17874db48c04
Dave Syer <dsyer@pivotal.io>
2019-12-16 14:10:08 +0000

    Simple upgrade to Spring Boot 2.2.2

--- atomikos-db/src/main/java/com/springsource/open/db/AtomikosApplication.java
+++ atomikos-db/src/main/java/com/springsource/open/db/AtomikosApplication.java
@@ -27,7 +27,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jta.XADataSourceWrapper;
+import org.springframework.boot.jdbc.XADataSourceWrapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
